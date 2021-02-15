16f41517fe657f07842063333c48c6da262181e2
Bruno Rosa <rosahbruno@gmail.com>
2021-02-15 16:53:51 -0500

    refactor(experience): convert experience to functional component

--- src/assets/constants/copy.js
+++ src/assets/constants/copy.js
@@ -14,11 +14,85 @@ export const copy = {
   },
   about: {
     header:
-      'Welcome to my site! I am a software engineer located in Pittsburgh, Pa. I am currently working towards my Master\'s of Data Science at Juniata College expected to graduate in Dec \'21.',
-    subHeader: 'I am currently working at Branding Brand as a technical lead of 3 apps for one of our largest clients. I am also a Co-Founder & Lead Developer for Precision Studio based out of York, Pa.',
-    tertiaryHeader: 'I have experience in Mobile, Web, Backend, & Data Engineering.'
+      "Welcome to my site! I am a software engineer located in Pittsburgh, Pa. I am currently working towards my Master's of Data Science at Juniata College expected to graduate in Dec '21.",
+    subHeader:
+      'I am currently working at Branding Brand as a technical lead of 3 apps for one of our largest clients. I am also a Co-Founder & Lead Developer for Precision Studio based out of York, Pa.',
+    tertiaryHeader:
+      'I have experience in Mobile, Web, Backend, & Data Engineering.'
   },
-  experience: {},
+  experience: [
+    {
+      title: 'Software Engineer',
+      startDate: "Dec '19",
+      endDate: 'Present',
+      company: 'Branding Brand',
+      description:
+        'Serving as Tech Lead on 3 React Native apps for one of our largest clients. I work very closely with our PMs, EMs, & Stakeholders to plan and complete work.\nI lead a team of 3-4 devs in maintenance and enhancements across the apps. During my time we have completed countless large features including an entire app redesign.\nI have also contributed to our process improvement by introducing more thorough code reviews, PR templates, unit testing, developer documentation, helping build a new release plan, etc.',
+      link: 'https://www.brandingbrand.com',
+      skills: 'Typescript, React Native, React, Jest, Travis, Git'
+    },
+    {
+      title: 'Co-Founder/Lead Developer',
+      startDate: "Jan '19",
+      endDate: 'Present',
+      company: 'Precision Studio',
+      description:
+        'I lead all technical projects and handle mobile/web development. We have worked with lots of clients in different capacities and rolled out 10+ sites.\nWe are also developing our own proprietary software which we intend to release in the near future!\nI also assist with with client acquisition, requirements gathering, etc.',
+      link: 'https://precisionstudio.org/',
+      skills:
+        'React, Go, Project Planning, Design, Marketing Campaigns, SEO, Git'
+    },
+    {
+      title: 'Software Engineer',
+      startDate: "Aug '19",
+      endDate: "Dec '19",
+      company: 'BNY Mellon',
+      description:
+        'I worked on the mobility team inside of workplace technology. My time was split between native (both iOS & Android) & hybrid development (Ionic).\nI led a major version upgrade of 2 of our hybrid apps in <1 month. I also worked with other internal teams on architecture and design of a new Emergency Contacts app.',
+      link: 'https://www.bnymellon.com/',
+      skills: 'Typescript, Swift, Kotlin, Ionic, Angular, Node, Sketch, GitLab'
+    },
+    {
+      title: 'Software Engineer',
+      startDate: "May '17",
+      endDate: "Aug '19",
+      company: 'UPMC',
+      description:
+        'I served as a member of the MARS application team which was a proprietary data warehouse that had over 30+ years of clinical health records.\nI split work between helping Sr devs get our old services ported over to our new Hadoop cluster and working on maintaining legacy Mars. I worked on a variety of things from Scala & Spark jobs, to API development in Node, automation scripting in Python, UI development in Polymer, and rewriting all of our developer documentation.\n',
+      link: 'https://www.upmc.com/',
+      skills: 'Python, Scala, Java, C/C++, Spark, Node, Hadoop, Linux, SQL, Git'
+    },
+    {
+      title: 'Lead Developer',
+      startDate: "Jan '18",
+      endDate: "Dec '19",
+      company: 'Bizi Marketplace',
+      description:
+        'Bizi was developed as a cross-platform marketplace app that targeted college campuses.\nI served as lead developer and architected and implemented all major features including: authentication, real-time messaging, notifications, social elements, product search, etc.\nOver the course of the project I managed 2 separate teams of 4-5 developers.',
+      link: 'https://www.bizimarketplace.com/',
+      skills: 'Typescript, Angular, Ionic, Node, Firebase, NoSQL, Git, Balsamiq'
+    },
+    {
+      title: 'Lead Web Developer',
+      startDate: "May '16",
+      endDate: "May '17",
+      company: 'Juniata College',
+      description:
+        "Led a team of 8 students that handled the frontend development of www.juniata.edu.\nOur team was responsible for meeting with campus offices and academic departments to discuss improvements and new designs for their sections of the site.\nI worked very closely with our Webmaster and Digital Marketing Specialist to ensure our team was following best practices and staying with the college's branding.",
+      link: 'https://www.juniata.edu',
+      skills: 'Javascript, PHP, SQL, Trello, OU Campus CMS'
+    },
+    {
+      title: 'Web Developer',
+      startDate: "Aug '15",
+      endDate: "May '16",
+      company: 'Juniata College',
+      description:
+        'Working as a web developer on the Marketing web team. I played a key role in migration of all site content from our old site to our new CMS.',
+      link: 'https://www.juniata.edu',
+      skills: 'Javascript, PHP, SQL, OU Campus CMS'
+    }
+  ],
   education: {},
   skills: {},
   interests: {},
