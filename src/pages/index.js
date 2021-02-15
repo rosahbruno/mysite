41a0c6fedc708febe523fca64e1464d58e4432f9
Bruno Rosa <rosahbruno@gmail.com>
2021-02-15 15:47:12 -0500

    feat: update styling on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,30 @@
 import React from 'react';
 
+// *** Components *** //
 import About from '../components/About';
 import Experience from '../components/Experience';
 import Education from '../components/Education';
 import Skills from '../components/Skills';
 import Interests from '../components/Interests';
 import Portfolio from '../components/Portfolio';
-
 import Layout from '../components/Layout';
 import Sidebar from '../components/Sidebar';
 
 const IndexPage = () => (
   <Layout>
-    <Sidebar/>
+    <Sidebar />
     <div className="container-fluid p-0">
-      <About/>
-      <hr className="m-0"/>
-      <Experience/>
-      <hr className="m-0"/>
-      <Education/>
-      <hr className="m-0"/>
-      <Skills/>
-      <hr className="m-0"/>
-      <Interests/>
-      <hr className="m-0"/>
-      <Portfolio/>
+      <About />
+      <hr className="m-0" />
+      <Experience />
+      <hr className="m-0" />
+      <Education />
+      <hr className="m-0" />
+      <Skills />
+      <hr className="m-0" />
+      <Interests />
+      <hr className="m-0" />
+      <Portfolio />
     </div>
   </Layout>
 );
