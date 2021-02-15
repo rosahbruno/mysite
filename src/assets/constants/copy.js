5ea09253b9cb33704e18818a33f542fa04443452
Bruno Rosa <rosahbruno@gmail.com>
2021-02-15 15:45:01 -0500

    feat(about): updates to about section

---
+++ src/assets/constants/copy.js
@@ -0,0 +1,26 @@
+export const copy = {
+  firstName: 'Bruno',
+  lastName: 'Rosa',
+  location: 'Pittsburgh, Pa',
+  myPhone: '(412) 773-1347',
+  myEmail: 'rosahbruno@gmail.com',
+  nav: {
+    about: 'About',
+    experience: 'Experience',
+    education: 'Education',
+    skills: 'Skills',
+    interests: 'Interests',
+    portfolio: 'Portfolio'
+  },
+  about: {
+    header:
+      'Welcome to my site! I am a software engineer located in Pittsburgh, Pa. I am currently working towards my Master\'s of Data Science at Juniata College expected to graduate in Dec \'21.',
+    subHeader: 'I am currently working at Branding Brand as a technical lead of 3 apps for one of our largest clients. I am also a Co-Founder & Lead Developer for Precision Studio based out of York, Pa.',
+    tertiaryHeader: 'I have experience in Mobile, Web, Backend, & Data Engineering.'
+  },
+  experience: {},
+  education: {},
+  skills: {},
+  interests: {},
+  portfolio: {}
+};
--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import config from '../../config';
+import { copy } from '../assets/constants/copy';
 
 const About = () => (
   <section
@@ -8,32 +9,28 @@ const About = () => (
   >
     <div className="w-100">
       <h1 className="mb-0">
-        {config.firstName}
-        <span className="text-primary"> {config.lastName}</span>
+        {copy.firstName}
+        <span className="text-primary"> {copy.lastName}</span>
       </h1>
       <div className="subheading mb-5">
-        {config.address} | <a href="tel:412-773-1347">{config.phone}</a> |
-        <a href={`mailto:${config.email}`}> {config.email}</a>
+        {copy.location} | <a href="tel:412-773-1347">{copy.myPhone}</a> |
+        <a href={`mailto:${copy.myEmail}`}> {copy.myEmail}</a>
       </div>
       <p className="lead mb-5">
-        welcome to my site! i am a software engineer from pittsburgh currently working <a
-        href="https://www.brandingbrand.com">@brandingbrand</a> specializing in mobile + web
-        development using react + react native.
-        <br/><br/>
-        i also am a co-founder + lead developer <a href="https://precisionstudio.org">@precisionstudio</a> where i
-        lead our web + mobile projects and help with design, marketing, client acquisition, etc..
-        <br/><br/>
-        and i've been building web sites for 5+ years and i have professional experience in big data & mobile dev
+        {copy.about.header}
+        <br />
+        <br />
+        {copy.about.subHeader}
+        <br />
+        <br />
+        {copy.about.tertiaryHeader}
       </p>
       <div className="social-icons">
-        {config.socialLinks.map(social => {
-          const { icon, url } = social;
-          return (
-            <a key={url} href={url}>
-              <i className={`fab ${icon}`}/>
-            </a>
-          );
-        })}
+        {config.socialLinks.map(social => (
+          <a key={social.url} href={social.url}>
+            <i className={`fab ${social.icon}`} />
+          </a>
+        ))}
       </div>
     </div>
   </section>
